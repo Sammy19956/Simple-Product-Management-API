dd3983df4fdfc7a50debd759de0bcb3d218184df
Sammy19956 <samson.ogberawhe@decagon.dev>
2023-02-18 10:15:40 +0100

    added endpoint to search by name filter

--- src/main/java/com/example/simpleproductmanagement/Services/ProductServiceImpl.java
+++ src/main/java/com/example/simpleproductmanagement/Services/ProductServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 @Service
 @AllArgsConstructor
-public class ProductServiceImpl implements ProductService{
+public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
     @Override
@@ -30,41 +30,68 @@ public class ProductServiceImpl implements ProductService{
     }
 
     @Override
-    public APIResponse<Product> updateProduct(Long productId, ProductDTO productDTO){
-       try{
-           Product product = productRepository.findById(productId)
-                   .orElseThrow(() -> new ProductNotFoundException("Product Not Found"));
+    public APIResponse<Product> updateProduct(Long productId, ProductDTO productDTO) {
+        try {
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new ProductNotFoundException("Product Not Found"));
 
-               product.setProductName(productDTO.getProductName());
-               product.setManufacturer(productDTO.getManufacturer());
-               product.setStatus(productDTO.getStatus());
-               product.setPrice(productDTO.getPrice());
-               product.setQuantityInStore(productDTO.getQuantityInStore());
-               product.setUpdatedAt(LocalDateTime.now());
-               productRepository.save(product);
-               return  new APIResponse<>(true, "Product updated successfully", product);
-       }catch (Exception ex){
-           return new APIResponse<>(true, ex.getMessage(),null);
-       }
+            product.setProductName(productDTO.getProductName());
+            product.setManufacturer(productDTO.getManufacturer());
+            product.setStatus(productDTO.getStatus());
+            product.setPrice(productDTO.getPrice());
+            product.setQuantityInStore(productDTO.getQuantityInStore());
+            product.setUpdatedAt(LocalDateTime.now());
+            productRepository.save(product);
+            return new APIResponse<>(true, "Product updated successfully", product);
+        } catch (Exception ex) {
+            return new APIResponse<>(true, ex.getMessage(), null);
+        }
     }
 
     @Override
-    public APIResponse<List<Product>> getAllProducts(){
+    public APIResponse<List<Product>> getAllProducts() {
         List<Product> products = productRepository.findAll();
         return new APIResponse<>(true, "Successful", products);
     }
 
     @Override
     public APIResponse<Product> getProductByCode(String uuid) {
-        try{
+        try {
             Product product = productRepository.findProductByProductCode(UUID.fromString(uuid));
-            if(product == null){
+            if (product == null) {
                 throw new ProductNotFoundException("Product with code " + uuid + " not found");
-            } else{
+            } else {
                 return new APIResponse<>(true, "Successful", product);
             }
 
-        }catch (Exception ex){
+        } catch (Exception ex) {
+            return new APIResponse<>(true, ex.getMessage(), null);
+        }
+
+    }
+
+    @Override
+    public APIResponse<Product> deleteProduct(Long id) {
+        try {
+            Product product = productRepository.findById(id)
+                    .orElseThrow(() -> new ProductNotFoundException("Product Not Found"));
+            productRepository.delete(product);
+            return new APIResponse<>(true, "Product deleted successfully", null);
+        } catch (Exception ex) {
+            return new APIResponse<>(true, ex.getMessage(), null);
+        }
+    }
+
+    @Override
+    public APIResponse<List<Product>> searchProductByName(String name) {
+        try {
+
+            List<Product> products = productRepository.findProductsByProductNameContaining(name);
+            if (products.size() == 0){
+                throw new ProductNotFoundException("There is no such product in our store");
+            }
+            return new APIResponse<>(true, "Successful",products);
+        } catch (Exception ex) {
             return new APIResponse<>(true, ex.getMessage(), null);
         }
 
--- src/main/java/com/example/simpleproductmanagement/repository/ProductRepository.java
+++ src/main/java/com/example/simpleproductmanagement/repository/ProductRepository.java
@@ -4,10 +4,12 @@ import com.example.simpleproductmanagement.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>
 {
     Product findProductByProductCode(UUID uuid);
+    List<Product> findProductsByProductNameContaining(String name);
 }
